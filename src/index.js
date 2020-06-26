3d42e8f258d2c605559cae53744a447a565f75dd
David <learnitmyway@users.noreply.github.com>
2020-06-26 09:33:36 +0200

    rename renderApp

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-window.renderApp = (containerId) => {
+window.renderReactApp = (containerId) => {
   ReactDOM.render(
     <React.StrictMode>
       <App />
