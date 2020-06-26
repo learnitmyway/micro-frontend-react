451aa09e04ca485c8db6e1420b3ff142ee028750
David <learnitmyway@users.noreply.github.com>
2020-06-26 09:02:35 +0200

    allow app to be served independently

--- src/index.js
+++ src/index.js
@@ -4,12 +4,12 @@ import './index.css'
 import App from './App'
 import * as serviceWorker from './serviceWorker'
 
-window.renderApp = () => {
+window.renderApp = (containerId) => {
   ReactDOM.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
-    document.getElementById('root')
+    document.getElementById(containerId)
   )
 }
 
