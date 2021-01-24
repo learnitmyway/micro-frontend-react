3198d3c73446b0496e3b1a7feec68260989328f2
David <learnitmyway@users.noreply.github.com>
2021-01-24 21:38:52 +0100

    autoformat app.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from 'react'
-import styles from './App.module.css'
-import Modal from 'react-modal'
+import React from "react";
+import styles from "./App.module.css";
+import Modal from "react-modal";
 
 const customStyles = {
   content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
   },
-}
+};
 
 function App({ containerId, resolve }) {
-  const [isOpen, setOpen] = React.useState(true)
-  Modal.setAppElement('#' + containerId)
-  var subtitle
+  const [isOpen, setOpen] = React.useState(true);
+  Modal.setAppElement("#" + containerId);
+  var subtitle;
 
   function afterOpenModal() {
     // references are now sync'd and can be accessed.
-    subtitle.style.color = '#f00'
+    subtitle.style.color = "#f00";
   }
 
   function closeModal(status) {
-    resolve(status)
-    setOpen(false)
+    resolve(status);
+    setOpen(false);
   }
 
   return (
@@ -37,16 +37,19 @@ function App({ containerId, resolve }) {
         style={customStyles}
         contentLabel="Example Modal"
       >
-        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>Hello, I'm an external app</h2>
-        <button onClick={() => closeModal('CANCEL')}>
-          cancel
-        </button>
-        <button className={styles.successButton} onClick={() => closeModal('SUCCESS')}>
+        <h2 ref={(_subtitle) => (subtitle = _subtitle)}>
+          Hello, I'm an external app
+        </h2>
+        <button onClick={() => closeModal("CANCEL")}>cancel</button>
+        <button
+          className={styles.successButton}
+          onClick={() => closeModal("SUCCESS")}
+        >
           success
         </button>
       </Modal>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
