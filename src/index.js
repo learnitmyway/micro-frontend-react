a4de3117d43696a36afecd66263a2ea1ff618c53
David <learnitmyway@users.noreply.github.com>
2021-01-24 22:02:10 +0100

    autoformat index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
-import * as serviceWorker from './serviceWorker'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 window.renderReactApp = (containerId) => {
   return new Promise((resolve, reject) => {
@@ -11,15 +11,15 @@ window.renderReactApp = (containerId) => {
         <App containerId={containerId} resolve={resolve} />
       </React.StrictMode>,
       document.getElementById(containerId)
-    )
-  })
-}
+    );
+  });
+};
 
 window.unmountReactApp = (containerId) => {
-  ReactDOM.unmountComponentAtNode(document.getElementById(containerId))
-}
+  ReactDOM.unmountComponentAtNode(document.getElementById(containerId));
+};
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister()
+serviceWorker.unregister();
